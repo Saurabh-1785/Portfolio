e439f313897b5d247b3f764bb1890cee6b9553ae
Saurabh-1785 <saurabhchauhan1785@gmail.com>
2025-07-24 11:23:15 +0000

    enhance it

--- script.js
+++ script.js
@@ -7,30 +7,3 @@ function hideSidebar() {
   const sidebar = document.querySelector('.sidebar');
   sidebar.style.display = 'none';
 }
-
-function openLiveDemo(button) {
-  const projectCell = button.closest('.project-cell');
-  const liveUrl = projectCell.getAttribute('data-live');
-  
-  if (liveUrl) {
-    window.open(liveUrl, '_blank');
-  } else {
-    // If no live URL is set, you can construct it from GitHub repo
-    const githubRepo = projectCell.getAttribute('data-github');
-    if (githubRepo) {
-      // You can customize this URL pattern based on your deployment service
-      const deployedUrl = `https://${githubRepo.replace('/', '-')}.vercel.app`;
-      window.open(deployedUrl, '_blank');
-    }
-  }
-}
-
-function openSourceCode(button) {
-  const projectCell = button.closest('.project-cell');
-  const githubRepo = projectCell.getAttribute('data-github');
-  
-  if (githubRepo) {
-    const githubUrl = `https://github.com/${githubRepo}`;
-    window.open(githubUrl, '_blank');
-  }
-}
